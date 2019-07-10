4cb2c409c030d1a67e04700a75ef8e9860755e56
Sonmi Kos <sonmi.kos@gmail.com>
2019-07-10 01:48:15 -0500

    added functioning  Next arrival, Minutes away logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,14 +20,14 @@ $("#submitbtn").on("click", function(event) {
     // Grabs user input
     var trainName = $("#nameInput").val().trim();
     var destinationInput = $("#destinationInput").val().trim();
-    var timeInput = ($("#timeInput").val().trim());
+    var firstTrainInput = ($("#firstTrainInput").val().trim());
     var frequencyInput = $("#frequencyInput").val().trim();
 
     // Creates local "temporary" object for holding train data
     var newTrain = {
         name: trainName,
         destination: destinationInput,
-        time: timeInput,
+        time: firstTrainInput,
         frequency: frequencyInput
     };
 
@@ -45,7 +45,7 @@ $("#submitbtn").on("click", function(event) {
     // Clears all of the text-boxes
     $("#nameInput").val("");
     $("#destinationInput").val("");
-    $("#timeInput").val("");
+    $("#firstTrainInput").val("");
     $("#frequencyInput").val("");
 });
 
@@ -56,13 +56,13 @@ database.ref().on("child_added", function(childSnapshot) {
     // Store everything into a variable.
     var trainName = childSnapshot.val().name;
     var destinationInput = childSnapshot.val().destination;
-    var timeInput = childSnapshot.val().time;
+    var firstTrainInput = childSnapshot.val().time;
     var frequencyInput = childSnapshot.val().frequency;
 
     // Train Info
     console.log(trainName);
     console.log(destinationInput);
-    console.log(timeInput);
+    console.log(firstTrainInput);
     console.log(frequencyInput);
 
     // Prettify the train time
@@ -72,23 +72,56 @@ database.ref().on("child_added", function(childSnapshot) {
     // To calculate next arrival
     // var nextArrival = moment().diff(moment(empStart, "X"), "months");
     // console.log(nextArrival);
-    var d = new Date();
-    var curr_hour = d.getHours();
-    var curr_min = d.getMinutes();
-    var timeNow = curr_hour + ":" + curr_min;
-    console.log(timeNow);
+
+
+    // var d = new Date();
+    // var curr_hour = d.getHours();
+    // var curr_min = d.getMinutes();
+    // var currentTime = curr_hour + ":" + curr_min;
+    // console.log(currentTime);
 
     // Calculate minutes away
     // var minAway;
     // console.log();
+
+
+    ///////////////////////////////////////////////////////////////
+
+
+    console.log(firstTrainInput);
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrainInput, "HH:mm").subtract(1, "years");;
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequencyInput;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var minAway = frequencyInput - tRemainder;
+    console.log("MINUTES AWAY: " + minAway);
+
+    // Next Train
+    var nextArrival = moment().add(minAway, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+
+    ///////////////////////////////////////////////////////////////
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(destinationInput),
         $("<td>").text(frequencyInput),
-        $("<td>").text("nextArrival"),
-        $("<td>").text("minAway"),
+        $("<td>").text(moment(nextArrival).format("hh:mm")),
+        $("<td>").text(minAway),
     );
 
     // Append the new row to the table
