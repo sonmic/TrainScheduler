23156020f2099acf9fffd2c301970a8aef5933aa
Sonmi Kos <sonmi.kos@gmail.com>
2019-07-11 00:53:45 -0500

    added validation to First Train Time input value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,147 +1,173 @@
+// Polyfill code for report validity for not compatible browsers
+if (!HTMLFormElement.prototype.reportValidity) {
+    HTMLFormElement.prototype.reportValidity = function() {
+        if (this.checkValidity()) return true;
+        var btn = document.createElement('button');
+        this.appendChild(btn);
+        btn.click();
+        this.removeChild(btn);
+        return false;
+    };
+}
+// Polyfill code ends
+
+
 // Fade in effect
 $(document).ready(function($) {
 
     window.setTimeout(function() {
         $(".cardbox").fadeIn(4000);
     }, 300); // 3 seconds
-});
-
-// slide effect
-// $(document).ready(function($) {
-
-//     window.setTimeout(function() {
-//         $("cardbox").show("slide", { direction: "left" }, 5000);
-//     }, 500); // 3 seconds
-// });
 
 
+    // slide effect
+    // $(document).ready(function($) {
 
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCPd9mQd9gUw_3P-wJQDteENtEccwjj1pA",
-    authDomain: "train-scheduler-74c3a.firebaseapp.com",
-    databaseURL: "https://train-scheduler-74c3a.firebaseio.com",
-    projectId: "train-scheduler-74c3a",
-    storageBucket: "",
-    messagingSenderId: "955634657286",
-    appId: "1:955634657286:web:0ac6c9d1503bac05"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    //     window.setTimeout(function() {
+    //         $("cardbox").show("slide", { direction: "left" }, 5000);
+    //     }, 500); // 3 seconds
+    // });
 
-var database = firebase.database();
 
-// Button for adding Trains
-$("#submitbtn").on("click", function(event) {
-    event.preventDefault();
-    console.log(true);
 
-    // Grabs user input
-    var trainName = $("#nameInput").val().trim();
-    var destinationInput = $("#destinationInput").val().trim();
-    var firstTrainInput = ($("#firstTrainInput").val().trim());
-    var frequencyInput = $("#frequencyInput").val().trim();
-
-    // Creates local "temporary" object for holding train data
-    var newTrain = {
-        name: trainName,
-        destination: destinationInput,
-        time: firstTrainInput,
-        frequency: frequencyInput
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCPd9mQd9gUw_3P-wJQDteENtEccwjj1pA",
+        authDomain: "train-scheduler-74c3a.firebaseapp.com",
+        databaseURL: "https://train-scheduler-74c3a.firebaseio.com",
+        projectId: "train-scheduler-74c3a",
+        storageBucket: "",
+        messagingSenderId: "955634657286",
+        appId: "1:955634657286:web:0ac6c9d1503bac05"
     };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-    // Uploads user input data to the database
-    database.ref().push(newTrain);
+    var database = firebase.database();
 
-    // Logs everything to console
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.time);
-    console.log(newTrain.frequency);
+    // Button for adding Trains
+    $("#submitbtn").on("click", function(event) {
+        event.preventDefault();
+        const form = document.querySelector('form');
+        if (!form.reportValidity()) {
+            return;
+        }
+        console.log(true);
+        $('#popup').bPopup({
+            appendTo: 'form',
+            zIndex: 2,
+            easing: 'easeOutBack', //uses jQuery easing plugin
+            speed: 450,
+            transition: 'slideDown',
+            opacity: 0.5,
+            modalColor: '#fff8ed'
+        });
+        // Grabs user input
+        var trainName = $("#nameInput").val().trim();
+        var destinationInput = $("#destinationInput").val().trim();
+        var firstTrainInput = ($("#firstTrainInput").val().trim());
+        var frequencyInput = $("#frequencyInput").val().trim();
+
+        // Creates local "temporary" object for holding train data
+        var newTrain = {
+            name: trainName,
+            destination: destinationInput,
+            time: firstTrainInput,
+            frequency: frequencyInput
+        };
+
+        // Uploads user input data to the database
+        database.ref().push(newTrain);
+
+        // Logs everything to console
+        console.log(newTrain.name);
+        console.log(newTrain.destination);
+        console.log(newTrain.time);
+        console.log(newTrain.frequency);
 
 
 
-    // alert("Train successfully added");
+        // alert("Train successfully added");
 
-    // Clears all of the text-boxes
-    $("#nameInput").val("");
-    $("#destinationInput").val("");
-    $("#firstTrainInput").val("");
-    $("#frequencyInput").val("");
+        // Clears all of the text-boxes
+        $("#nameInput").val("");
+        $("#destinationInput").val("");
+        $("#firstTrainInput").val("");
+        $("#frequencyInput").val("");
+    });
+
+    // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+
+        // Store everything into a variable.
+        let trainName = childSnapshot.val().name;
+        let destinationInput = childSnapshot.val().destination;
+        let firstTrainInput = childSnapshot.val().time;
+        let frequencyInput = childSnapshot.val().frequency;
+
+        // Train Info
+        console.log(trainName);
+        console.log(destinationInput);
+        console.log(firstTrainInput);
+        console.log(frequencyInput);
+
+        ///////////////////////////////////////////////////////////////
+
+
+        console.log(firstTrainInput);
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        let firstTimeConverted = moment(firstTrainInput, "HH:mm").subtract(1, "years");;
+        console.log(firstTimeConverted);
+
+        ///////////////////////////////////////////////////////////////
+
+        let nextArrivalCell = $("<td>");
+        let minAwayCell = $("<td>");
+
+        function updateTimes() {
+            // Current Time
+            let currentTime = moment();
+            console.log("CURRENT TIME: " + currentTime.format("hh:mm"));
+
+            // Difference between the times
+            let diffTime = currentTime.diff(firstTimeConverted, "minutes");
+            console.log("DIFFERENCE IN TIME: " + diffTime);
+
+            // Time apart (remainder)
+            let tRemainder = diffTime % frequencyInput;
+            console.log(tRemainder);
+
+            // Minute Until Train
+            let minAway = frequencyInput - tRemainder;
+            console.log("MINUTES AWAY: " + minAway);
+
+            // Next Train
+            let nextArrival = currentTime.add(minAway, "minutes");
+            console.log("ARRIVAL TIME: " + nextArrival.format("hh:mm"));
+
+            nextArrivalCell.text(nextArrival.format("hh:mm"));
+            minAwayCell.text(minAway);
+        }
+        updateTimes();
+
+        // Create the new row
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destinationInput),
+            $("<td>").text(frequencyInput),
+            nextArrivalCell,
+            minAwayCell
+        );
+
+        // time auto refresh
+        setInterval(updateTimes, 60 * 1000);
+
+        // Append the new row to the table
+        $("#traintable > tbody").append(newRow);
+    });
+
 });
-
-// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-
-    // Store everything into a variable.
-    let trainName = childSnapshot.val().name;
-    let destinationInput = childSnapshot.val().destination;
-    let firstTrainInput = childSnapshot.val().time;
-    let frequencyInput = childSnapshot.val().frequency;
-
-    // Train Info
-    console.log(trainName);
-    console.log(destinationInput);
-    console.log(firstTrainInput);
-    console.log(frequencyInput);
-
-    ///////////////////////////////////////////////////////////////
-
-
-    console.log(firstTrainInput);
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    let firstTimeConverted = moment(firstTrainInput, "HH:mm").subtract(1, "years");;
-    console.log(firstTimeConverted);
-
-    ///////////////////////////////////////////////////////////////
-
-    let nextArrivalCell = $("<td>");
-    let minAwayCell = $("<td>");
-
-    function updateTimes() {
-        // Current Time
-        let currentTime = moment();
-        console.log("CURRENT TIME: " + currentTime.format("hh:mm"));
-
-        // Difference between the times
-        let diffTime = currentTime.diff(firstTimeConverted, "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
-
-        // Time apart (remainder)
-        let tRemainder = diffTime % frequencyInput;
-        console.log(tRemainder);
-
-        // Minute Until Train
-        let minAway = frequencyInput - tRemainder;
-        console.log("MINUTES AWAY: " + minAway);
-
-        // Next Train
-        let nextArrival = currentTime.add(minAway, "minutes");
-        console.log("ARRIVAL TIME: " + nextArrival.format("hh:mm"));
-
-        nextArrivalCell.text(nextArrival.format("hh:mm"));
-        minAwayCell.text(minAway);
-    }
-    updateTimes();
-
-    // Create the new row
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(destinationInput),
-        $("<td>").text(frequencyInput),
-        nextArrivalCell,
-        minAwayCell
-    );
-
-    // time auto refresh
-    setInterval(updateTimes, 60 * 1000);
-
-    // Append the new row to the table
-    $("#traintable > tbody").append(newRow);
-});
-
-
 // scroll btn
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -175,20 +201,8 @@ function display_ct() {
     display_c();
 }
 
-
-
-// Popup animation
-(function($) {
-    $('#submitbtn').bind('click', function(e) {
-        e.preventDefault();
-        $('#popup').bPopup({
-            appendTo: 'form',
-            zIndex: 2,
-            easing: 'easeOutBack', //uses jQuery easing plugin
-            speed: 450,
-            transition: 'slideDown',
-            opacity: 0.5,
-            modalColor: '#fff8ed'
-        });
-    });
-})(jQuery);
+// form first train time input validation
+let input = document.getElementById('#firstTrainInput');
+input.oninvalid = function(event) {
+    event.target.setCustomValidity('Username should only contain lowercase letters. e.g. john');
+}
